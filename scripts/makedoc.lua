f14b6558eb94a319d6a19ab4625de23d8dc3fc67
Gaspard Bucher <gaspard@teti.ch>
2013-04-04 10:08:16 +0200

    Releasing 1.0.2 compatible with Lua 5.2.

--- scripts/makedoc.lua
+++ scripts/makedoc.lua
@@ -1,17 +1,16 @@
-local lub = require 'lub'
-lub.Doc.make {
+local lut = require 'lut'
+lut.Doc.make {
   sources = {
     'lub',
   },
-  head   = '<meta name="google-site-verification" content="p1ZrKNheIo5xMhrMOo9MxKaY2hL9LlyuPYyRnEl2QuM" />',
   target = 'doc',
   format = 'html',
-  header = [[ <a href='http://lubyk.org'><img alt='lubyk logo' src='img/lubyk.png'/></a> <h1><a href='http://doc.lubyk.org'>Lubyk documentation</a></h1></a> ]],
+  header = [[<h1><a href='http://doc.lubyk.org'>Lubyk documentation</a></h1></a> ]],
   index  = [=[
 --[[--
   # Lubyk documentation
 
-  ## List of available modules in lubyk
+  ## List of available modules
 --]]--
 ]=]
 }
