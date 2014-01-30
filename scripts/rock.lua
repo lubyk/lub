c95331a5a36c354a47e41922cda5d25140c22f2b
Gaspard Bucher <gaspard@teti.ch>
2014-01-30 20:05:26 +0100

    Removing cmake dependency.

--- scripts/rock.lua
+++ scripts/rock.lua
@@ -1,13 +1,13 @@
 #!/usr/bin/env lua
 local lub = require 'lub'
+
 local lib = lub
 
 local tmp = lub.Template(lub.content(lub.path '|rockspec.in'))
-lub.writeall(lib.type..'-'..lib.VERSION..'-1.rockspec', tmp:run({lib = lib}))
-
-tmp = lub.Template(lub.content(lub.path '|CMakeLists.txt.in'))
-lub.writeall('CMakeLists.txt', tmp:run())
+lub.writeall(lib.type..'-'..lib.VERSION..'-1.rockspec', tmp:run {lib = lib})
 
 tmp = lub.Template(lub.content(lub.path '|dist.info.in'))
-lub.writeall('dist.info', tmp:run({lib = lib}))
+lub.writeall('dist.info', tmp:run {lib = lib})
+
+
 
